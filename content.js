0bad3ae35048c9dfedd93568e5d6e81a82e4f82b
GhitaB <ghitabizau27@gmail.com>
2021-07-29 16:32:32 +0300

    Improve structure and design.

---
+++ content.js
@@ -0,0 +1,56 @@
+var all_quotes = [
+    {
+        text: "Întărește adevărul și dreptatea în copiii tăi.",
+        source: "Învățăturile lui Ptahhotep, 597",
+        tags: ["adevăr", "dreptate", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Respectă adevărul și nu trece peste el.",
+        source: "Învățăturile lui Ptahhotep, 151",
+        tags: ["adevăr", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Glasul tău să nu fie aspru, nu vorbi din toată tăria limbii tale",
+        source: "Papirus Insinger, XXII, 10",
+        tags: ["vorbire", "blândețe", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Mai bună este pâinea mâncată cu inima fericită, decât avuție cu mâhnire.",
+        source: "Învățăturile lui Amen-em-Ope, IV",
+        tags: ["fericire", "viață", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Nu se poate ști ce este în inima omului.",
+        source: "Povestea țăranului bun de gură",
+        tags: ["inimă", "om", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Nu te simți deznădăjduit în lume când ești slab și lipsit, căci acela ce este slab poate să fie tare din nou.",
+        source: "Papirus Insinger, XIX, 16-17",
+        tags: ["încurajare", "motivație", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Nu răspunde la bine prin rău.",
+        source: "Povestea țăranului bun de gură",
+        tags: ["bine", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "Firea blândă și bună a unui om ales îi este de mare folos.",
+        source: "Învățăturile lui Ptahhotep, 493",
+        tags: ["blândețe", "bunătate", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+    {
+        text: "O fire blândă va lăsa o amintire fericită în lume.",
+        source: "Învățăturile lui Ptahhotep, 494",
+        tags: ["blândețe", "egiptenii-antici"],
+        published: "2021-07-28"
+    },
+];
